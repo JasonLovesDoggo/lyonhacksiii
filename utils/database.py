cb4453e082cc02c6bb037bfe90ec2d7bcf714d02
Jason <jasoncameron.all@gmail.com>
2023-04-07 16:11:24 -0400

    idk

--- utils/database.py
+++ utils/database.py
@@ -1,65 +1,38 @@
 import sqlite3
+from datetime import datetime
 
 
-class Database:
+class ReminderDatabase:
     def __init__(self):
-        self.con = sqlite3.connect("todo.db")
-        self.cursor = self.con.cursor()
-        self.create_task_table()
+        self.conn = sqlite3.connect('reminders.db')
+        self.create_table()
 
-    def create_task_table(self):
-        """Create tasks table"""
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS tasks(id integer PRIMARY KEY AUTOINCREMENT, task varchar(50) NOT NULL, due_date varchar(50), completed BOOLEAN NOT NULL CHECK (completed IN (0, 1)))"
-        )
-        self.con.commit()
+    def create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS reminders
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, due_date TIMESTAMP)
+        ''')
+        self.conn.commit()
 
-    def create_task(self, task, due_date=None):
-        """Create a task"""
-        self.cursor.execute(
-            "INSERT INTO tasks(task, due_date, completed) VALUES(?, ?, ?)",
-            (task, due_date, 0),
-        )
-        self.con.commit()
+    def add_reminder(self, title, due_date):
+        cursor = self.conn.cursor()
+        cursor.execute('INSERT INTO reminders (title, due_date) VALUES (?, ?)', (title, due_date))
+        self.conn.commit()
 
-        # GETTING THE LAST ENTERED ITEM SO WE CAN ADD IT TO THE TASK LIST
-        created_task = self.cursor.execute(
-            "SELECT id, task, due_date FROM tasks WHERE task = ? and completed = 0",
-            (task,),
-        ).fetchall()
-        return created_task[-1]
+    def get_reminders(self):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM reminders ORDER BY due_date DESC')
+        rows = cursor.fetchall()
+        reminders = [{'id': row[0], 'title': row[1], 'due_date': datetime.fromisoformat(row[2])} for row in rows]
+        return reminders
 
-    def get_tasks(self):
-        """Get tasks"""
-        uncomplete_tasks = self.cursor.execute(
-            "SELECT id, task, due_date FROM tasks WHERE completed = 0"
-        ).fetchall()
-        completed_tasks = self.cursor.execute(
-            "SELECT id, task, due_date FROM tasks WHERE completed = 1"
-        ).fetchall()
+    def delete_reminder(self, reminder_id):
+        cursor = self.conn.cursor()
+        cursor.execute('DELETE FROM reminders WHERE id = ?', (reminder_id,))
+        self.conn.commit()
 
-        return completed_tasks, uncomplete_tasks
-
-    def mark_task_as_complete(self, taskid):
-        """Marking tasks as complete"""
-        self.cursor.execute("UPDATE tasks SET completed=1 WHERE id=?", (taskid,))
-        self.con.commit()
-
-    def mark_task_as_incomplete(self, taskid):
-        """Mark task as uncomplete"""
-        self.cursor.execute("UPDATE tasks SET completed=0 WHERE id=?", (taskid,))
-        self.con.commit()
-
-        # return the text of the task
-        task_text = self.cursor.execute(
-            "SELECT task FROM tasks WHERE id=?", (taskid,)
-        ).fetchall()
-        return task_text[0][0]
-
-    def delete_task(self, taskid):
-        """Delete a task"""
-        self.cursor.execute("DELETE FROM tasks WHERE id=?", (taskid,))
-        self.con.commit()
-
-    def close_db_connection(self):
-        self.con.close()
+    def update_reminder(self, reminder_id, title, due_date):
+        cursor = self.conn.cursor()
+        cursor.execute('UPDATE reminders SET title = ?, due_date = ? WHERE id = ?', (title, due_date, reminder_id))
+        self.conn.commit()
